70efc3d96c760837d8ca07c3eb2bd94526a38be2
Batuhan <yahsibatuhan@gmail.com>
2017-10-27 12:22:55 +0300

    first commit

---
+++ PMS.Data/Models/Test.cs
@@ -0,0 +1,10 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace PMS.Data.Models
+{
+    class Test
+    {
+    }
+}
---
+++ PMS.Extension/Extensions/Test.cs
@@ -0,0 +1,10 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace PMS.Extension.Extensions
+{
+    class Test
+    {
+    }
+}
